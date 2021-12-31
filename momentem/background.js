29e7d96f106fa5d0e97296f064d1d7dba9f9a491
kangaroo19 <1000jjj@naver.com>
2021-12-31 17:43:13 +0900

    123

--- momentem/background.js
+++ momentem/background.js
@@ -1,5 +1,5 @@
 const body=document.querySelector("body");
-const images=["0.jpg","1.jpg","2.jpg"];
+const images=["retro1.jpg","retro2.png","retro3.jpg","retro4.jpg","retro5.png"];
 
 const chosenImage=images[Math.floor(Math.random()*images.length)];
 
